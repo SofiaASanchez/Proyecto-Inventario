789dec861cb88d1ccfdc5887a1a3dddcca6f10a0
Kanakku24 <bcanaca@gmail.com>
2018-12-08 08:10:59 +0100

    Interfaz + conexion

--- Inventario/src/inventario/Inventario.java
+++ Inventario/src/inventario/Inventario.java
@@ -5,7 +5,8 @@
  */
 package inventario;
 
-import Interfaz.UI_Login;
+    import Interfaz.UI_Login;
+    import bdpostgresql.database_inventario;
 
 public class Inventario {
 
@@ -13,9 +14,13 @@ public class Inventario {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        UI_Login Fram = new UI_Login();
-        Fram.setVisible(true);
-        Fram.setLocationRelativeTo(null);
-    }
+        
+        database_inventario db = new database_inventario();
+        if (db.conectarme() == true){
+            UI_Login Fram = new UI_Login();
+            Fram.setVisible(true);
+            Fram.setLocationRelativeTo(null);
+        }
+     }
     
 }
