12e7dba8b9cb8378ff130a05d26fb2a60888b6a6
Kanakku24 <bcanaca@gmail.com>
2018-12-08 03:04:31 +0100

    Interfaz, actualizacion pdf

---
+++ Inventario/src/inventario/Inventario.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inventario;
+
+import Interfaz.UI_Login;
+
+public class Inventario {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        UI_Login Fram = new UI_Login();
+        Fram.setVisible(true);
+        Fram.setLocationRelativeTo(null);
+    }
+    
+}
